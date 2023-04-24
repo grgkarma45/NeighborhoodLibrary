4f43ce694008cb7535bdb6f0a8bb8872e5ac441a
grgkarma45 <grgkarma45@gmail.com>
2023-04-24 14:44:30 -0400

    Initial commit

---
+++ src/main/java/NeighborhoodLibrary/Books.java
@@ -0,0 +1,50 @@
+package NeighborhoodLibrary;
+
+public class Books {
+    private int id;
+    private String isbn;
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+}
---
+++ src/main/java/NeighborhoodLibrary/Main.java
@@ -0,0 +1,24 @@
+package NeighborhoodLibrary;
+
+import java.util.Scanner;
+//import java.lang.ClassNotFoundException: Main;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Invite the user to buy something
+        System.out.println("Hello! Welcome to my store. Would you like to purchase something");
+        while (true) {
+            // Read in the user's response
+            String userResponse = scanner.nextLine();
+            if (userResponse.equals("yes")) {
+                System.out.println("Great! Here are the list of items available");
+            } else if (userResponse.equals("no")) {
+                System.out.println("Thanks for stopping by!");
+                return;
+            }
+        }
+    }
+}
